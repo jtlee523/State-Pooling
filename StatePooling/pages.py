672124c8f2b6e8ae3618d48e4cf4f9d4502689b2
Joseph Lee <josephlee@dyn-160-39-172-229.dyn.columbia.edu>
2019-03-07 13:58:44 -0500

    Initial commit

---
+++ StatePooling/pages.py
@@ -0,0 +1,45 @@
+from otree.api import Currency as c, currency_range
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+#FOR TASK 1
+
+class Task1_Page1(Page):
+	form_model = 'player'
+	form_fields=['Var6', 'Var0','Var1', 'Var2','Var3','Var4','Var5' ]
+	pass
+
+
+class Task1_PageRED(Page):
+	pass	
+
+class Task1_PageGREEN(Page):
+	pass	
+	
+class Task1_PageYELLOW(Page):
+	pass	
+	
+	
+class Test(Page):
+    pass
+
+
+
+
+class ResultsWaitPage(WaitPage):
+
+    def after_all_players_arrive(self):
+        pass
+
+
+class Results(Page):
+    pass
+
+
+
+page_sequence = [
+	Task1_Page1,
+	Task1_PageRED,
+	Task1_PageGREEN,
+	Task1_PageYELLOW,
+]
